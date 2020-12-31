0350e24d254ebebc352ca0f83a5dc7829936dcfc
microwerx <microwerx@outlook.com>
2020-12-31 12:15:02 -0800

    change pip3 to python3 -m pip

--- pi/mlaptpip.sh
+++ pi/mlaptpip.sh
@@ -4,26 +4,26 @@
 python3 -m venv --system-site-packages mlpi
 source ./mlpi/bin/activate
  
-pip3 install --upgrade pip
-pip3 install setuptools
+python3 -m pip install --upgrade pip
+python3 -m pip install setuptools
  
 cowsay "Installing Python Packages"
 # Machine Learning Tools
-pip3 install jupyter
-pip3 install jupyterlab
-pip3 install pandas
-pip3 install seaborn
-# pip3 install torch
-pip3 install scikit-image
-pip3 install mtcnn
-pip3 install scipy
-pip3 install imutils
-pip3 install json_minify
+python3 -m pip install jupyter
+python3 -m pip install jupyterlab
+python3 -m pip install pandas
+python3 -m pip install seaborn
+# python3 -m pip install torch
+python3 -m pip install scikit-image
+python3 -m pip install mtcnn
+python3 -m pip install scipy
+python3 -m pip install imutils
+python3 -m pip install json_minify
  
 cowsay "Installing OpenCV"
-#pip3 install picamera[array]
-pip3 install opencv-contrib-python
-pip3 install mtcnn
+#python3 -m pip install picamera[array]
+python3 -m pip install opencv-contrib-python
+python3 -m pip install mtcnn
  
 # cowsay "Let's set up a password"
 # python3 jupyter password
@@ -33,15 +33,15 @@ exit 0
 
 cowsay "Installing Tensorflow"
 # Tensorflow + Keras
-pip3 install tensorflow-cpu
-# pip3 install tensorflow-tensorboard
-# pip3 install tensorflow-hub
-pip3 install tfds-nightly
-pip3 install keras-tuner
-pip3 install keras-vggface
+python3 -m pip install tensorflow-cpu
+# python3 -m pip install tensorflow-tensorboard
+# python3 -m pip install tensorflow-hub
+python3 -m pip install tfds-nightly
+python3 -m pip install keras-tuner
+python3 -m pip install keras-vggface
 
 # cowsay "Installing online tools"
 # # Install from web
-pip3 install git+https://github.com/tensorflow/docs
+python3 -m pip install git+https://github.com/tensorflow/docs
 
 deactivate
